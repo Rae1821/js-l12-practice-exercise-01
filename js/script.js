7dca7f13eb07063942b176eb6433878a9c2a327a
Rae1821 <102691550+Rae1821@users.noreply.github.com>
2022-04-13 11:53:45 -0400

    Completed Exercise 2

--- js/script.js
+++ js/script.js
@@ -1,40 +1,40 @@
 const randomFolks = document.querySelector(".random-peeps");
+const selectUserNumber = document.querySelector("#users");
 
-const getData = async function () {
-    const usersRequest = await fetch("https://randomuser.me/api?results=5");
+const getData = async function (numUsers) {
+    const usersRequest = await fetch(`https://randomuser.me/api/?results=${numUsers}`);
     const data = await usersRequest.json();
          
+const userResults = data.results;
+displayUsers(userResults);
 };
 
 getData();
 
-let userResults = results.map(function (userResults) {
-    return userResults;
-    console.log(userResults);
-    displayUsers(userResults);
-});
-
 
 const displayUsers = function (userResults) {
-    userResults = [];
     randomFolks.innerHTML = "";
     
-    for (let user of userResults) {
-        const country = user.location["country"];
-        const name = user.name["first"];
-        const imageURL = user.picture["medium"];
-        }
-    const userDiv = document.createElement("div");
+    for (const user of userResults) {
+        const country = user.location.country;
+        const name = user.name.first;
+        const imageURL = user.picture.medium;
+        const userDiv = document.createElement("div");
         userDiv.innerHTML = `
                 <h3>${name}</h3>
                 <p>${country}</p>
                 <img src=${imageURL} alt="User avatar" />
                 `;
         randomFolks.append(userDiv);
-        const hide = document.querySelector(".num-users hide");
-        hide.classList.remove("hide");
-        hide.classList.add("show");
-
     }
+      
+};
 
+selectUserNumber.addEventListener("change", function (e) {
+    const numUsers = e.target.value;
+    getData(numUsers);
+});
+
+   
     
+
